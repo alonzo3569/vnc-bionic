973b4ad98a04313de71c899ed653d1e510d61abc
logan xps vnc <r07525074@ntu.edu.tw>
2021-04-24 11:29:11 +0000

    verify fft & rewrite visulization upload

--- catkin_ws/src/signal_process/src/fft.py
+++ catkin_ws/src/signal_process/src/fft.py
@@ -41,6 +41,8 @@ class FFT:
             ch1 = np.array(msg.data_ch1)
             ch2 = np.array(msg.data_ch2)
             #print "ch1 shape : ", ch1.shape #(192000,)
+            #time_start = rospy.get_time()
+            #print "time_start : ", time_start
 
             # Append data to array 
             self.ch1_data_array = np.concatenate((self.ch1_data_array,ch1))
@@ -62,19 +64,8 @@ class FFT:
                 psd1 = 20 * np.log10(psd1)
                 psd2 = 20 * np.log10(psd2)
                 #print "psd1 : ", psd1.shape #(513,)
-
-                # Publish fft data
-                self.fft_msg.header = Header(frame_id=FFT.FRAME_ID, stamp=rospy.Time.now())
                 self.fft_msg.fft_ch1.extend(psd1.tolist()) # len(fft_ch1) = 513 
                 self.fft_msg.fft_ch2.extend(psd2.tolist())
-                self.fft_msg.fs = self.fs
-                self.fft_msg.delta_f = self.fs / float(self.chunk)
-                self.fft_msg.delta_t = (float(self.chunk) / self.fs)*(1 - self.overlap)
-                self.pub.publish(self.fft_msg)
-
-                # clear after publish
-                del self.fft_msg.fft_ch1[:]
-                del self.fft_msg.fft_ch2[:]
 
                 # Remove data
                 self.ch1_data_array = self.ch1_data_array[int(self.chunk*(1-self.overlap)):]
@@ -82,8 +73,25 @@ class FFT:
 
                 self.count = self.count + 1
 
-            print "Finish process ", self.count, " chunks of data."
+            # Publish fft data
+            self.fft_msg.header = Header(frame_id=FFT.FRAME_ID, stamp=rospy.Time.now())
+            self.fft_msg.header = Header(stamp=rospy.Time.now())
+            self.fft_msg.fs = self.fs
+            self.fft_msg.delta_f = self.fs / float(self.chunk)
+            self.fft_msg.delta_t = (float(self.chunk) / self.fs)*(1 - self.overlap)
+            self.pub.publish(self.fft_msg)
+            #print"Shape of msg.fft_ch2 : ", np.array(self.fft_msg.fft_ch2).shape (965466,)
+
+            # clear after publish
+            del self.fft_msg.fft_ch1[:]
+            del self.fft_msg.fft_ch2[:]
+
+
+            print "Finish process ", self.count, " chunks of data." # process 1sec data in 0.2sec
             self.count = 0
+            #time_end = rospy.get_time() 
+            #print "time_end : ", time_end
+            #print "Process 1s data spent : ", time_end-time_start, " sec"
 
 
 
--- catkin_ws/src/visualization/src/plot.py
+++ catkin_ws/src/visualization/src/plot.py
@@ -18,73 +18,63 @@ class PLOT:
         # OnstartUp
         self.chunk = rospy.get_param('~chunk', 1024)
         self.fs = rospy.get_param('~fs', 192000)
-        #print "Set Format   : ", str(self.format), type(self.format)
 
-        # Initialize
-        self.init_fft = False
+        # Initialize fig
+        self.fig, self.ax = plt.subplots(figsize=(10,3))
+        self.plot_fft_ch1 = np.zeros((3,3))
+        self.im = self.ax.imshow(self.plot_fft_ch1, cmap=plt.cm.jet, origin='lower', vmin=0, vmax=-100, aspect='auto')
+        self.fig.colorbar(self.im, ax=self.ax)
+        #self.ax.set_label_position()
+        self.ax.set_xlabel("Time(s)")
+        self.ax.set_ylabel("Frequency(Hz)")
+
+        # Initialize params
+        self.first_msg = True
         self.delta_f = 0.0
         self.delta_t = 0.0
         self.counter = 0
-        self.plot_fft_ch1 = np.zeros((10,10))
-        #self.fft_msg = HydrophoneFFTData()
-        self.fig, self.ax = plt.subplots(1, figsize=(10,3))
+        self.time_pre = 0.0
+        self.fft_result_len = self.chunk//2 + 1
 
 
         # Subscriber
-        #rospy.Subscriber("/hydrophone_data", HydrophoneData, self.hydro_cb)
         rospy.Subscriber("/fft", HydrophoneFFTData, self.fft_cb)
 
-        # Publisher
-        #self.pub = rospy.Publisher("/fft", HydrophoneFFTData, queue_size=10)
+
 
     def fft_cb(self, msg):
+        # Calculate msg recieved
+        self.counter = self.counter + 1
+        time_cur = rospy.get_time()
+        if time_cur - self.time_pre >= 1.0:
+            print(f'Recieved {self.counter} msgs in 1 sec')
+            self.counter = 0
+            self.time_pre = time_cur
 
-        # Initialize fft plot frame
-        if self.init_fft == False:
-            print('init===========================================================')
+        # Initialize plot frame base on msg
+        if self.first_msg == True:
             self.delta_f = msg.delta_f
             self.delta_t = msg.delta_t
-            #print(f'delta_t : {self.delta_t}')
-            #print(f'cal : {int(PLOT.PLOT_FFT_TIME//self.delta_t)}')
-            self.plot_fft_ch1 = np.zeros((len(msg.fft_ch1),int(PLOT.PLOT_FFT_TIME//self.delta_t))) # (513,9375)
-            #print(f'shape of plot array : {self.plot_fft_ch1.shape}') 
-            self.fft_f_axis = np.arange(0,self.fs//2+self.delta_f,self.delta_f) # +self.delta_f for 2n+"1" # f_axis shape (513,)
-            #print(f'shape of f_axis : {self.fft_f_axis.shape}')
-            self.init_fft = True
+            self.plot_fft_ch1 = np.zeros((self.fft_result_len,int(PLOT.PLOT_FFT_TIME//self.delta_t)))
+            #print(f'shape of plot array : {self.plot_fft_ch1.shape}') # (513, 9375)
+            self.first_msg = False
 
-        ch1 = np.array(msg.fft_ch1)[:,np.newaxis] + 170
-        #print(f'ch1 shape (513,1): {ch1.shape}') # ch1.shape (513,1)
+        # Subscribe fft data and change into 2 dimension array
+        ch1 = np.array(msg.fft_ch1).reshape(int(len(msg.fft_ch1)/self.fft_result_len),self.fft_result_len).T # +211 #(513, 1882)
 
-        # roll & replace previous data
-        #self.plot_fft_ch1 = np.roll(self.plot_fft_ch1,-1,0)
-        #print(f'plot shape : {self.plot_fft_ch1.shape}') # ch1.shape (513, 1875)(plot 1s)
-        #self.plot_fft_ch1[:,-1] = ch1
-        #print(f'self.plot_fft_ch1[:,-1], {self.plot_fft_ch1[:,-1].shape}') #(513,)
-        self.plot_fft_ch1 = self.plot_fft_ch1[:,1:] # remove left column # (513,9374)
-        self.plot_fft_ch1 = np.concatenate((self.plot_fft_ch1,ch1),axis=1) # (513,9375)
+        # Remove previous data and append new data
+        self.plot_fft_ch1 = self.plot_fft_ch1[:,ch1.shape[1]:] # (513, 7493)
+        self.plot_fft_ch1 = np.concatenate((self.plot_fft_ch1,ch1),axis=1) # (513, 9375)
+
+
+
+    def animation_frame(self,i):
+        self.im.set_array(self.plot_fft_ch1)
+        self.im.set_extent([0,5,0,96])
+        return [self.im]
+
+
         
-
-        # time axis
-        self.counter = self.counter + 1
-        if self.counter < self.plot_fft_ch1.shape[1]: # 9375
-            self.fft_t_axis = np.arange(0,PLOT.PLOT_FFT_TIME-self.delta_t,self.delta_t)
-            #print(f'shape of t_axis : {self.fft_t_axis.shape}') # t_axis shape (9375,)
-        else:
-            self.fft_t_axis = np.arange(0,PLOT.PLOT_FFT_TIME-self.delta_t,self.delta_t) + (self.counter-self.plot_fft_ch1.shape[1])*self.delta_t
-
-
-
-    def animate(self,i):
-        self.ax.clear()
-    
-        self.ax.imshow(self.plot_fft_ch1, origin="lower",cmap=plt.cm.jet)#, extent=[self.fft_t_axis[-1]-PLOT.PLOT_FFT_TIME, self.fft_t_axis[-1], 0, self.fft_f_axis[-1]])
-        self.ax.axis('auto')
-        self.ax.set_xlabel("Time(s)")
-        self.ax.set_ylabel("Frequency(Hz)")
-        #self.ax[0][0].set_ylim([0, 20000])
-    
-
-
     def onShutdown(self):
         rospy.loginfo("FFT node Shutdown.")
 
@@ -92,7 +82,7 @@ class PLOT:
 if __name__ == '__main__':
     rospy.init_node('plot')
     node = PLOT()
-    ani = FuncAnimation(node.fig, node.animate, interval=1000)
+    animation = FuncAnimation(node.fig, node.animation_frame, interval=500, blit=True)
     rospy.on_shutdown(node.onShutdown)
     plt.show()
     rospy.spin()
